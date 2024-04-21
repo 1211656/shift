2b429fef99a108efb604920d1f1ad99ac1d1e5e9
1211656 <105354083+1211656@users.noreply.github.com>
2024-04-21 05:34:18 +0100

    some things

--- shiftAppens/src/domain/client/Client.py
+++
@@ -1,5 +0,0 @@
-class Client(db.Model):
-   __tablename__ = "Client"
-   id = db.Column(db.Integer, primary_key=True)
-   email = db.Column(Email, unique=True, nullable=False)
-   password = db.Column(Password, unique=True, nullable=False)
